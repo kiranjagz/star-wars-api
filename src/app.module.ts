357b99f6358bf0fa3932eaecc9209c30835290e0
Kiran Jagathlal <kiranjagz@gmail.com>
2021-04-05 19:45:30 +0200

    Updated readme and added planets controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PeopleController } from './people/people.controller';
 import { PeopleService } from './people/people.service';
+import { PlanetsController } from './planets/planets.controller';
 
 @Module({
   imports: [HttpModule],
-  controllers: [AppController, PeopleController],
+  controllers: [AppController, PeopleController, PlanetsController],
   providers: [AppService, PeopleService],
 })
 export class AppModule {}
